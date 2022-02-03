722de83bd31228379c4748ec99130d6de9be06d6
jdacevedol <jdacevedol@eafit.edu.co>
2022-02-02 22:16:47 -0500

    agregacion resta y division +extras

--- codigoPy.py
+++ codigoPy.py
@@ -1,16 +1,29 @@
-from traceback import print_tb
-
-
-def multiplicacion(num):
-    sum = num * 5
+def resta(num1, num2):
+    sum = num1 - num2
     return sum
 
-def suma(num):
-    sum = num + 5
+
+def division(num1, num2):
+    sum = num1 * num2
     return sum
+
+
+def multiplicacion(num1, num2):
+    mul = num1 * num2
+    print("Su multiplicacion es: ",  mul)
+
+
+def suma(num1, num2):
+    sum = num1 + num2
+    print("Su suma es: ", )
 
 
 if __name__ == '__main__':
-    num = int(input("Ingrese un numero: "))
-    print("Su multiplicacion es: ",  multiplicacion(num))
-    print("Su suma es: ", suma(num))
+    num1 = int(input("Ingrese un numero 1: "))
+    num2 = int(input("Ingrese un numero 2: "))
+    multiplicacion(num1, num2)
+    suma(num1, num2)
+    resta(num1, num2)
+    division(num1, num2)
+    
+    
